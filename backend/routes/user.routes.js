9e0de395caa9fd7df8945cda7473678c9d874dae
SeniboDonPedro <senibodonpedro@gmail.com>
2024-02-09 14:42:53 +0100

    minor import corrections

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,8 @@
 import express from 'express'
 
 const router = express.Router()
+import protectRoute from '../middleware/protectRoute.js'
+import { getUsersForSidebar } from '../controllers/userController.js'
 
 router.get('/', protectRoute, getUsersForSidebar)
 
